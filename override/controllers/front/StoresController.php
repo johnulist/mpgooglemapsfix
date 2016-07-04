3400bc81ec927882caa335c4897b54f18322bc4a
Michael Dekker <git@michaeldekker.nl>
2016-07-04 11:57:47 +0200

    Initial commit

--- override/controllers/front/StoresController.php
+++ override/controllers/front/StoresController.php
@@ -3,13 +3,16 @@ class StoresController extends StoresControllerCore
 {
     public function setMedia()
     {
-        parent::setMedia();
+        if (!Module::isEnabled('mpgooglemapsfix')) {
+            return parent::setMedia();
+        }
+        FrontController::setMedia();
         $this->addCSS(_THEME_CSS_DIR_.'stores.css');
 
         if (!Configuration::get('PS_STORES_SIMPLIFIED')) {
             $default_country = new Country((int)Tools::getCountry());
             $googleMapsJs = 'http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?sensor=true&region='.substr($default_country->iso_code, 0, 2);
-            if (Module::isEnabled('mpgooglemapsfix') && Configuration::get('MPGOOGLEMAPSFIX_API_KEY')) {
+            if (Configuration::get('MPGOOGLEMAPSFIX_API_KEY')) {
                 $googleMapsJs .= '&'.rawurlencode(Configuration::get('MPGOOGLEMAPSFIX_API_KEY'));
             }
             $this->addJS($googleMapsJs);
